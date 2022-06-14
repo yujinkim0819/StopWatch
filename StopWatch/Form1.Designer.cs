0becae5560188db390cf75719647f800a160858c
Yujin Kim <a76363282@gmail.com>
2022-06-14 10:18:30 +0900

    윈폼으로 스톱워치 만들기

--- StopWatch/Form1.Designer.cs
+++ StopWatch/Form1.Designer.cs
@@ -29,12 +29,185 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.리셋ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.나만의스톱워치정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem1,
+            this.도움말ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(497, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem1
+            // 
+            this.파일ToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.리셋ToolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.끝내기ToolStripMenuItem});
+            this.파일ToolStripMenuItem1.Name = "파일ToolStripMenuItem1";
+            this.파일ToolStripMenuItem1.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem1.Text = "파일";
+            this.파일ToolStripMenuItem1.Click += new System.EventHandler(this.파일ToolStripMenuItem1_Click);
+            // 
+            // 리셋ToolStripMenuItem1
+            // 
+            this.리셋ToolStripMenuItem1.Name = "리셋ToolStripMenuItem1";
+            this.리셋ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.리셋ToolStripMenuItem1.Text = "리셋";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            // 
+            // 도움말ToolStripMenuItem
+            // 
+            this.도움말ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.나만의스톱워치정보ToolStripMenuItem});
+            this.도움말ToolStripMenuItem.Name = "도움말ToolStripMenuItem";
+            this.도움말ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.도움말ToolStripMenuItem.Text = "도움말";
+            // 
+            // 나만의스톱워치정보ToolStripMenuItem
+            // 
+            this.나만의스톱워치정보ToolStripMenuItem.Name = "나만의스톱워치정보ToolStripMenuItem";
+            this.나만의스톱워치정보ToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.나만의스톱워치정보ToolStripMenuItem.Text = "나만의 스톱워치 정보";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 438);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(497, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(122, 17);
+            this.toolStripStatusLabel1.Text = "나만의 스톱워치 v0.1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(136, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "나만의 스톱워치";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnStart.Font = new System.Drawing.Font("굴림", 49F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(129, 140);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(232, 117);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(127, 281);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(234, 31);
+            this.lblStatus.TabIndex = 4;
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Enabled = false;
+            this.tbStatus.Location = new System.Drawing.Point(129, 346);
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.ReadOnly = true;
+            this.tbStatus.Size = new System.Drawing.Size(232, 21);
+            this.tbStatus.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(497, 460);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "나만의 스톱워치";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 리셋ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 나만의스톱워치정보ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- StopWatch/Form1.cs
+++ StopWatch/Form1.cs
@@ -12,9 +12,46 @@ namespace StopWatch
 {
     public partial class Form1 : Form
     {
+        private int elapsedTime = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 파일ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.elapsedTime++;
+            this.lblStatus.Text = this.elapsedTime + "초 경과";
+            this.tbStatus.Text = this.elapsedTime + "초 경과";
+            this.toolStripProgressBar1.Value = this.elapsedTime;
+            if(this.toolStripProgressBar1.Maximum == this.toolStripProgressBar1.Value)
+            {
+                timer1.Enabled = false; 
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled=false;
+                btnStart.Text = "시작";
+            } else
+            {
+                timer1.Enabled = true;
+                btnStart.Text = "스톱";
+            }
         }
     }
 }
